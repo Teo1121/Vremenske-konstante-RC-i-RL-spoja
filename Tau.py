aa68c9525b8fadfc4f08b35c04707016a802ed14
Teo Ferenac <teo.ferenac@gmail.com>
2020-02-14 18:24:37 +0100

    Made it look like an actual simulation

--- Tau.py
+++ Tau.py
@@ -1,13 +1,14 @@
 import pygame
 import pygame.gfxdraw
 import numpy as np
-import matplotlib.backends.backend_agg as agg
+import matplotlib.backends.backend_agg as agg # use own implementation
 import pylab
+from time import time
 
 pygame.init()
 
-displaySize = (1200,800)
-surfaceSize = (1200,2200)
+displaySize = (1200,950)
+surfaceSize = (1200,2150)
 surface = pygame.surface.Surface(surfaceSize)
 font = pygame.font.SysFont("monospace",16)
 scroll_y = 0
@@ -49,23 +50,36 @@ class otpornik:
         self.x = x # pozicija X
         self.y = y # pozicija Y
         self.r = r # otpor u Ohm
-        
+
     def draw(self): # metoda za crtanje simbola
         text.print(self.r,self.x+14,self.y-22,(0,0,255))
 
         pygame.draw.rect(surface,(0,0,0),(self.x,self.y-7,28,14),1) # tijelo
-        
 class kondenzator:
     def __init__(self,x,y,c):
         self.x = x # pozicija X
         self.y = y # pozicija Y
         self.c = c # kapacitet u F
-        
+
+        self.I = 0
+        self.U = 0
     def draw(self): # metoda za crtanje
         text.print(self.c,self.x+3,self.y-22,(255,0,0))
 
         pygame.draw.line(surface,(0,0,0),(self.x,self.y-10),(self.x,self.y+10),1) # gornja crta
         pygame.draw.line(surface,(0,0,0),(self.x+6,self.y-10),(self.x+6,self.y+10),1) # doljnja crta
+
+        # smijer struje
+        pygame.draw.line(surface,(220,100,20),(self.x+int(self.I/3),self.y+16),(self.x-int(self.I/3),self.y+16))
+        pygame.draw.rect(surface,(220,100,20),(self.x+int(self.I/3),self.y+14,5,5))
+        
+    def update(self,state,t):
+        if state: # punjenje
+            self.U = 100*(1-np.e**(-t/1)) # napon u postocima i tau u intervalima od 1 s
+            self.I = 100*np.e**(-t/1)
+        else:
+            self.U = 100*np.e**(-t/1)
+            self.I = -100*np.e**(-t/1)
         
 class zavojnica:
     def __init__(self,x,y,l):
@@ -80,6 +94,18 @@ class zavojnica:
         pygame.gfxdraw.arc(surface,self.x+18,self.y+6,6,180,0,(0,0,0))
         pygame.gfxdraw.arc(surface,self.x+30,self.y+6,6,180,0,(0,0,0))
         pygame.gfxdraw.arc(surface,self.x+42,self.y+6,6,180,0,(0,0,0))
+
+        # smijer struje
+        pygame.draw.line(surface,(220,100,20),(self.x+24+int(self.I/3),self.y+16),(self.x+24-int(self.I/3),self.y+16))
+        pygame.draw.rect(surface,(220,100,20),(self.x+22+int(self.I/3),self.y+14,5,5))
+        
+    def update(self,state,t):
+        if state: # punjenje
+            self.I = 100*(1-np.e**(-t/1)) # napon u postocima i Tau u intervalima od 1 s
+            self.U = 100*np.e**(-t/1)
+        else:
+            self.I = 100*np.e**(-t/1)
+            self.U = -100*(1-np.e**(-t/1))
         
 class wire:
     def __init__(self,points,w=2):
@@ -95,19 +121,21 @@ class text:
         self.x = x
         self.y = y
         self.text = text
-        self.input = ""
+        self.input = "" 
         self.string = font.render(text,True,color)
+    # metoda za ispis    
     def pr(self,center = False):
         if center:
             rect = self.string.get_rect()
             surface.blit(self.string,(self.x-rect.width//2,self.y-rect.height//2))
         else:
             surface.blit(self.string,(self.x,self.y))
+    # staticka matoda         
     def print(text,x,y,color=(0,0,0),center=True):
         text = font.render(text,True,color)
         rect = text.get_rect()
         surface.blit(text,(x-rect.width//2, y-rect.height//2) if center else (x,y))
-        
+     # metode za upis    
     def removelast(self):
         self.input = self.input[:-1]
         self.string = font.render(self.text+self.input,True,(0,0,0))
@@ -127,12 +155,15 @@ class graph:
         self.h = h # visina
         self.fig = pylab.figure(figsize=(w/dpi,h/dpi), 
                    dpi=dpi, )
-        
-        self.chargingPoz = [((-0.368**x)+1)*100 for x in np.linspace(0,6,100)]
-        self.chargingNeg = [((0.368**x)-1)*100  for x in np.linspace(0,6,100)]
 
-        self.dischargingPoz = [(0.368**x)*100 for x in np.linspace(0,6,100)]
-        self.dischargingNeg = [(-0.368**x)*100  for x in np.linspace(0,6,100)]
+        self.precision = 100
+        self.time = np.linspace(0,6,self.precision)
+        
+        self.chargingPoz = [((-0.368**x)+1)*100 for x in self.time]
+        self.chargingNeg = [((0.368**x)-1)*100  for x in self.time]
+
+        self.dischargingPoz = [(0.368**x)*100 for x in self.time]
+        self.dischargingNeg = [(-0.368**x)*100  for x in self.time]
         
         self.data = None
         self.size = None
@@ -142,10 +173,10 @@ class graph:
             graph = pygame.image.fromstring(self.data, self.size, "RGB")
             surface.blit(graph,(self.x,self.y))
         
-    def update(self,charging,tp):
+    def update(self,charging,tp,sec):
         self.fig.clf()
         ax = self.fig.gca()
-        ax.set_xlabel("Vrijeme (Tao)", fontsize=12)
+        ax.set_xlabel("Vrijeme (Tau)", fontsize=12)
         ax.set_ylabel("Napon (% od maksimalnog)",fontsize=12)
         ax.hlines(0, xmin=0, xmax=6, color='black')
         
@@ -153,22 +184,22 @@ class graph:
             ax.vlines(1, ymin=0, ymax=100, color='gray')
             if tp == "RC":
                 ax.hlines(63.2, xmin=0, xmax=6, color='gray')
-                ax.plot(np.linspace(0,6,100),self.dischargingPoz)
-                ax.plot(np.linspace(0,6,100),self.chargingPoz)
+                ax.plot(self.time[:int(sec*self.precision/6)],self.dischargingPoz[:int(sec*self.precision/6)])
+                ax.plot(self.time[:int(sec*self.precision/6)],self.chargingPoz[:int(sec*self.precision/6)])
             elif tp == "RL":
                 ax.hlines(36.8, xmin=0, xmax=6, color='gray')
-                ax.plot(np.linspace(0,6,100),self.chargingPoz)
-                ax.plot(np.linspace(0,6,100),self.dischargingPoz)
+                ax.plot(self.time[:int(sec*self.precision/6)],self.chargingPoz[:int(sec*self.precision/6)])
+                ax.plot(self.time[:int(sec*self.precision/6)],self.dischargingPoz[:int(sec*self.precision/6)])
         else:
             ax.vlines(1, ymin=-100, ymax=100, color='gray')
             if tp == "RC":
                 ax.hlines(36.8, xmin=0, xmax=6, color='gray')
-                ax.plot(np.linspace(0,6,100),self.dischargingNeg)
-                ax.plot(np.linspace(0,6,100),self.dischargingPoz)
+                ax.plot(self.time[:int(sec*self.precision/6)],self.dischargingNeg[:int(sec*self.precision/6)])
+                ax.plot(self.time[:int(sec*self.precision/6)],self.dischargingPoz[:int(sec*self.precision/6)])
             elif tp == "RL":
                 ax.hlines(-36.8, xmin=0, xmax=6, color='gray')
-                ax.plot(np.linspace(0,6,100),self.dischargingPoz)
-                ax.plot(np.linspace(0,6,100),self.dischargingNeg)
+                ax.plot(self.time[:int(sec*self.precision/6)],self.dischargingPoz[:int(sec*self.precision/6)])
+                ax.plot(self.time[:int(sec*self.precision/6)],self.dischargingNeg[:int(sec*self.precision/6)])
 
         canvas = agg.FigureCanvasAgg(self.fig)
         canvas.draw()
@@ -176,6 +207,7 @@ class graph:
         
         self.size = canvas.get_width_height()
         self.data = renderer.tostring_rgb()
+
 
 def Input(string):
     typing = True
@@ -211,11 +243,31 @@ def validInput(string):
             continue
         if ret != 0.0:
             return ret
+        
 def update():
+    global scroll_y
     sklopkaRC.update()
     sklopkaRL.update()
-    RCgraf.update(sklopkaRC.on,"RC")
-    RLgraf.update(sklopkaRL.on,"RL")
+    
+    start = time()
+    while time() - start < 6:
+        kondenzator.update(sklopkaRC.on,time()-start)
+        zavojnica.update(sklopkaRL.on,time()-start)
+        RCgraf.update(sklopkaRC.on,"RC",time()-start)
+        RLgraf.update(sklopkaRL.on,"RL",time()-start)
+        for e in pygame.event.get():
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                if e.button == 4:
+                    scroll_y = 0
+                elif e.button == 5:
+                    scroll_y = displaySize[1]-surfaceSize[1]
+        draw()
+    kondenzator.update(sklopkaRC.on,6)
+    zavojnica.update(sklopkaRL.on,6)
+    RCgraf.update(sklopkaRC.on,"RC",6)
+    RLgraf.update(sklopkaRL.on,"RL",6)
+    draw()
+    
     
 def draw():
     surface.fill((255,255,255))
@@ -227,7 +279,7 @@ def draw():
     for zica in RCzice:
         zica.draw()
         
-    TaoRC.pr()
+    TauRC.pr()
     RCgraf.draw()
     
     granica.draw()
@@ -239,8 +291,11 @@ def draw():
     for zica in RLzice:
         zica.draw()
 
-    TaoRL.pr()
+    TauRL.pr()
     RLgraf.draw()
+
+    display.blit(surface,(0,scroll_y))
+    pygame.display.update()
 
 display = pygame.display.set_mode((800,22))
 
@@ -253,13 +308,13 @@ pygame.display.init()
 
 display = pygame.display.set_mode(displaySize)
 
-TaoRC = float(otpor*kapacitet)
-TaoRC = "Jedan tao u RC spoju traje "+str(TaoRC)+" sec"
-TaoRC = text(TaoRC,20,120)
+TauRC = float(otpor*kapacitet)
+TauRC = "Jedan Tau u RC spoju traje "+str(TauRC)+" sec"
+TauRC = text(TauRC,20,120)
 
-TaoRL = induktivitet/otpor
-TaoRL = "Jedan tao u RL spoju traje "+str(TaoRL)+" sec"
-TaoRL = text(TaoRL,20,1320)
+TauRL = induktivitet/otpor
+TauRL = "Jedan Tau u RL spoju traje "+str(TauRL)+" sec"
+TauRL = text(TauRL,20,1320)
 
 izvorRC = izvor(20,70,"10V")
 otpornikRC = otpornik(150,35,str(otpor)+" Ohm")
@@ -294,8 +349,6 @@ RLgraf = graph(20,1350,1200,800)
 
 update()
 draw()
-display.blit(surface,(0,scroll_y))
-pygame.display.update()
 
 while True:
     for e in pygame.event.get():
@@ -314,6 +367,5 @@ while True:
             if e.key == pygame.K_SPACE:
                 update()
                 draw()
-                display.blit(surface,(0,scroll_y))
-                pygame.display.update()
+                
                     
